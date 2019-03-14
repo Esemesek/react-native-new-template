413d3c9b139df17050db76b14e173e332bad7f4b
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2019-03-14 17:39:17 +0100

    Template config

---
+++ template.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "ProjectName",
+
+  // Directory of template within repository
+  templateDir: "./template",
+
+  // Path to script to be executed in postInit
+  postInit: undefined
+};
--- template/index.js
+++ template/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const APP_NAME = 'ProjectName';
+
+AppRegistry.registerComponent(APP_NAME, () => App);
